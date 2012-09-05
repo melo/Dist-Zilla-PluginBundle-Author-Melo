74b253fb6009ba14fc36bc11e579457873d7ea12
Pedro Melo <melo@simplicidade.org>
2012-09-05 18:11:43 +0100

    Generate a .travis.yml on all projects using us
    The generate .travis.yml file will make Travis-ci install this bundle and
    then use dzil to test the distribution, to include all the release tests.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Dist/Zilla/PluginBundle/Author/MELO.pm
+++ lib/Dist/Zilla/PluginBundle/Author/MELO.pm
@@ -157,11 +157,10 @@ method configure {
 
     # gather and prune
     $self->_generate_manifest_skip,
-    qw(
-      GatherDir
-      PruneCruft
-      ManifestSkip
-      ),
+    $self->_generate_travis_yml,
+    'GatherDir',
+    [PruneCruft => { except => ['\.travis.yml'] }],
+    'ManifestSkip',
 
     # this is just for github
     # TODO: still not sure this is a good idea - if metacpan.org used that on
@@ -319,7 +318,7 @@ foreach my $method (qw(log log_fatal)) {
   }
 }
 
-sub _generate_manifest_skip {
+method _generate_manifest_skip {
 
   # include a default MANIFEST.SKIP for the tests and/or historical reasons
   return [
@@ -342,6 +341,32 @@ sub _generate_manifest_skip {
 ^.DS_Store$
 
 EOF_MANIFEST_SKIP
+    }
+  ];
+}
+
+method _generate_travis_yml {
+
+  # include a .travis.yml: required if we want to smoke our build/* and
+  # releases branches
+  return [
+    GenerateFile => 'GenerateTravisCfg' => {
+      filename => '.travis.yml',
+      content  => <<EOF_TRAVIS_CFG,
+language: perl
+perl:
+  - "5.16"
+  - "5.14"
+  - "5.12"
+  - "5.10"
+before_install:
+  - "git config --global github.user melo"
+  - "cpanm --quiet --notest Dist::Zilla"
+  - "cpanm --quiet --notest --installdeps Dist::Zilla::PluginBundle::Author::MELO"
+  - "cpanm --quiet --notest Dist::Zilla::PluginBundle::Author::MELO"
+install: "dzil authordeps | xargs cpanm --quiet --notest && dzil listdeps | xargs cpanm --quiet --notest"
+script: "dzil test --release"
+EOF_TRAVIS_CFG
     }
   ];
 }
