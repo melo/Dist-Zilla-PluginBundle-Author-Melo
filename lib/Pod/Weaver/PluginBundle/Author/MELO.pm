1f79926a3a132b91b4cb04298afe1f47d60811fc
Sergey Romanov <complefor@rambler.ru>
2013-11-20 16:32:15 +0400

    require P::W at least 4.000

--- lib/Pod/Weaver/PluginBundle/Author/MELO.pm
+++ lib/Pod/Weaver/PluginBundle/Author/MELO.pm
@@ -10,7 +10,7 @@ BEGIN {
 
 use strict;
 use warnings;
-use Pod::Weaver 3.101633 ();
+use Pod::Weaver 4.000 ();
 use Pod::Weaver::PluginBundle::Default ();
 use Pod::Weaver::Plugin::StopWords 1.001005 ();
 use Pod::Weaver::Plugin::Transformer ();
